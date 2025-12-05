c734e4b1024fa3f8ce8259fdd535eeffe82ddf05
ART <art@ARTui-MacBookPro.local>
2025-12-05 11:08:33 +1100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/FirstView.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Festival Pulse" />
+</template>
+
+<style scoped>
+
+</style>
